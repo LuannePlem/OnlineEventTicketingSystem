2a340a48f12536c3d50ad87c2d965d6419a433e0
Mikayla <mikayla@thehubbards.org>
2025-09-19 15:11:55 -0400

    add url, register app, and add home.html

---
+++ OTS_main/OTS_APP/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+# from django.contrib.auth import views as auth
+
+from django.contrib import admin  # Django admin module
+from django.conf import settings   # Application settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # Static files serving
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+
+] 
+#  Serve media files if DEBUG is True (development mode)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files using staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
