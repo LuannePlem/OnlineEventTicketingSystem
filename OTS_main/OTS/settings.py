de526b0922f521f2b56c7d121e112a9c1ed074f3
Luanne <l.plemmons1@andersonuniversity.edu>
2025-09-19 14:42:32 -0400

    Changed database from SQLite to PostgreSQL

--- OTS_main/OTS/settings.py
+++ OTS_main/OTS/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': '12345678',
+        'HOST': 'w3-django-project.cdxmgq9zqqlr.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
