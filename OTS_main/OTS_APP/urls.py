398d618efc9c8931face5b729ccd385288a00cdf
Mikayla <mikayla@thehubbards.org>
2025-10-24 09:30:38 -0400

    Registeration, Login, and logout functionality

--- OTS_main/OTS_APP/urls.py
+++ OTS_main/OTS_APP/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path
 from . import views
-# from django.contrib.auth import views as auth
+# from django.contrib.
+# auth import views as auth
 
 from django.contrib import admin  # Django admin module
 from django.conf import settings   # Application settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # Static files serving
 from django.conf.urls.static import static
+from .views import *
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
+    path('register/', views.register, name="register"),
+    path('landingPage/', views.landingPage, name="landingPage"),
+    path('upcoming_events/', views.upcoming, name="upcoming"),
+    path('current_events/', views.current, name="current"),
+    path('individual_event/', views.individualEvent, name="individualEvent"),
+    # path('create_events/', views.createEvent, name="createEvent"),
+    # path('edit_events/', views.editEvent, name="editEvent"),
+    
+    
 ] 
 #  Serve media files if DEBUG is True (development mode)
 if settings.DEBUG:
